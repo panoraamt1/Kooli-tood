48dea9a728483a5ddd81803007f5d473a8ee30ee
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-01-23 13:33:48 +0200

    ForLoop

---
+++ ForLoopTwo/ForLoopTwo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ForLoopTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ruudu tegemine");
+
+            Console.WriteLine("Sisesta ruudu suurus.");
+            int squareSize = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= squareSize; row++)
+            {
+                for (int column = 1; column <= squareSize; column++)
+                {
+                    string variable =(row == column || row + column == squareSize +1) ? " X " : " 0 ";
+
+                    Console.Write(variable);
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloForLoop/HelloForLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int tere = 3;
+
+            for (int i = 0; i < tere; i++)
+            {
+                Console.WriteLine("Tere tulemast.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
