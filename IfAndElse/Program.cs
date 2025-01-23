b1ed46fe8b8b686c780df9b793fef58232c29c7a
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-01-23 11:21:06 +0200

    Esimene proov

---
+++ IfAndElse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+
+            int num = Convert.ToInt32 (Console.ReadLine());
+
+            if (num <= 0)
+            {
+                Console.WriteLine("No can do mate.");
+            }
+
+            else
+            {
+                Console.WriteLine("Be ye " + num * 3 + ", I see, I see.");
+            }
+        }
+    }
+}
---
+++ MCore/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage esimene arv:");
+            double earv = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisestage teine arv:");
+            double tarv = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisestage tehe:");
+            string tehe = (Console.ReadLine());
+
+            if (tehe == "+")
+            {
+                Console.WriteLine("Tulemus: " + earv + "+" + tarv + "=" + (earv + tarv));
+            }
+            else if (tehe == "-")
+            {
+                Console.WriteLine("Tulemus: " + earv + "-" + tarv + "=" + (earv - tarv));
+            }
+            else if (tehe == "*")
+            {
+                Console.WriteLine("Tulemus: " + earv + "*" + tarv + "=" + (earv * tarv));
+            }
+            else if (tehe == "/" && tarv == 0)
+            {
+                Console.WriteLine("Can't devide with 0");
+            }
+            else if (tehe == "/")
+            {
+                Console.WriteLine("Tulemus: " + earv + "/" + tarv + "=" + (earv / tarv));
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
