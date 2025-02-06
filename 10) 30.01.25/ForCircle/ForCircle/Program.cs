6038978afc55e253d41633f3da0e7942606f0c3b
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-02-06 11:51:24 +0200

    30.01.25

---
+++ 10) 30.01.25/ForCircle/ForCircle/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ForCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            double radius;
+            double thickness = 0.4;
+            char symbil = '*';
+
+            do 
+            {
+                Console.Write("Enter radius:::");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Radius has to be a positive number.");
+                }
+            } 
+            while (radius <= 0);
+
+            Console.WriteLine();
+            double rIn = radius - thickness, rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; y--)
+            {
+                
+                for (double x = -radius; x < rOut; x+= 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(symbil);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 10) 30.01.25/ForLoopShape/ForLoopShape/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ForLoopShape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage püramiidi suurus.");
+            int i, j, n;
+            Console.WriteLine("-----------------------------------------------------");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.Write('\n');
+
+
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}",j);
+                }
+                for (j = i-1; j >= 1; j--)
+                {
+                    Console.Write("{0}",j);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
