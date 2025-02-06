d6e3b73952c115a92234ee207d25fb8efa9f9635
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-02-06 11:29:10 +0200

    30.01.25

---
+++ 10) 30.01.25/ForTriangelNumber/ForTriangelNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForTriangelNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbrite kolmnurk!");
+
+            int i, j, rows;
+
+            Console.WriteLine("Sisestage ridade arv.");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 1; i <= rows; i++)
+            {
+                for ( j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ 10) 30.01.25/ForTriangleNumber2/ForTriangleNumber2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForTriangleNumber2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int i, j, rows;
+
+            Console.WriteLine("Sisesta kolmnurga suurus.");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
